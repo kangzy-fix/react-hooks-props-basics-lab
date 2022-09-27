ba21f1a5b5adf436fd580db56ae9b29da6ec9b9e
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-09-27 12:08:48 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import Links from "./Links";
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 
 function App() {
